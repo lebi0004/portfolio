360f273f9d85e8648e56d44babe4c3c1e150c7b2
lebi0004 <155406508+lebi0004@users.noreply.github.com>
2024-09-21 11:49:52 -0400

    Delete Portfolio directory

--- Portfolio/arrow.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-    const scrollButton = document.querySelector('.scroll-to-top');
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      scrollButton.style.display = "block";
-    } else {
-      scrollButton.style.display = "none";
-    }
-  };
-  
